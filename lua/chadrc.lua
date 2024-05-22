b5145d72af7984d3d3dc337bf921edb500000739
zhangxh-math <cheungyf.cn+github@gmail.com>
2024-05-22 10:38:30 +0800

    Update header

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -14,7 +14,6 @@ M.ui = {
 	-- },
 	nvdash = {
 		load_on_startup = true,
-	
 		header = {
 			"⠀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⡴⢫⢅⣫⣾⣟⣿⣦⣝⣻⣯⠵⠛⠛⠛⠻⠿⣟⡿⣶⣅⡚⢭⡙⠿⢿⣿⣿⣿⣿⣿⣿⣿⣯⡻⢿⣿⣿⣿⣿⣿⣿⣿⣽⡽⣿⣿⣿",
 			"⠂⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣼⠟⡌⣣⣾⣿⠷⢛⠉⢀⡼⡃⠀⠀⠀⠀⠀⠒⠤⣀⠙⠻⣾⣻⡳⣌⢇⡢⢍⡛⢿⣿⣿⣿⣿⣿⣿⣷⡹⣿⣿⣿⣿⣿⣿⣿⣿⣝⣿⣿",
@@ -54,16 +53,7 @@ M.ui = {
 			"⣿⣞⣷⣿⣿⣯⡻⣄⠀⠀⠀⠀⠀⠀⣠⠔⢁⠼⡇⠀⠀⠘⡀⠘⡄⠙⡷⡄⠐⢡⠤⠤⠤⢄⡀⢸⠨⢱⠘⣆⠪⢆⡀⠀⠀⠰⡀⢸⡅⢺⡜⣣⠙⡴⣉⢏⡞⣥⠻⠀⠀⠀⠀⠀⠀",
 			"⣿⣿⣿⣿⣿⣿⣿⣮⡛⣤⣀⣀⡠⣊⠤⠂⠀⠀⢹⠀⠀⠀⢇⠀⢣⠀⠘⠌⠒⠤⡷⠁⠀⠀⠎⠁⠄⠃⠜⠀⠳⢌⠻⠆⠤⠤⠌⢀⠇⠣⡔⠣⢙⠲⢤⠣⠜⢢⠛⠆⠀⠀⠀⠀⠀",
 		},
-	
-		buttons = {
-		  { "  Find File", "Spc f f", "Telescope find_files" },
-		  { "󰈚  Recent Files", "Spc f o", "Telescope oldfiles" },
-		  { "󰈭  Find Word", "Spc f w", "Telescope live_grep" },
-		  { "  Bookmarks", "Spc m a", "Telescope marks" },
-		  { "  Themes", "Spc t h", "Telescope themes" },
-		  { "  Mappings", "Spc c h", "NvCheatsheet" },
-		},
-	  },
+	},
 }
 
 return M
