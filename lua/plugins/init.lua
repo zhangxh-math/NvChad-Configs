5b78d940bf131391c60189dc1cf416cc558e9147
zhangxh-math <zhangxh.math@gmail.com>
2024-05-28 01:18:45 +0800

    优化配置
    1. 删除保存和撤销快捷键
    2. 自动设置路径为打开的文件所在的文件夹
    3. 添加Lazygit.nvim 插件

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -28,6 +28,25 @@ return {
     end,
   },
   {
+    "kdheepak/lazygit.nvim",
+    cmd = {
+      "LazyGit",
+      "LazyGitConfig",
+      "LazyGitCurrentFile",
+      "LazyGitFilter",
+      "LazyGitFilterCurrentFile",
+    },
+  -- optional for floating window border decoration
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+  -- setting the keybinding for LazyGit with 'keys' is recommended in
+  -- order to load the plugin when the command is run for the first time
+    keys = {
+      { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" }
+    }
+  },
+  {
     "nvim-treesitter/nvim-treesitter",
     event = "BufReadPost",  -- 使用 BufReadPost 事件懒加载
     config = {
