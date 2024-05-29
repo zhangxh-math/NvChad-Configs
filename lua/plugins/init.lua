1a2b7e1b3677ac4449ca5899660fc6da766caee9
zhangxh-math <zhangxh.math@gmail.com>
2024-05-29 09:37:25 +0800

    修改Mac的Vimtex插件选项

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -15,6 +15,8 @@ return {
       vim.g.vimtex_view_method = 'skim'
       vim.g.vimtex_complete_enabled = 1
       vim.g.vimtex_syntax_enabled = 1
+      vim.g.vimtex_view_skim_sync = 1
+      vim.g.vimtex_view_skim_activate = 1
       vim.g.vimtex_quickfix_mode = 0
     end,
   },
